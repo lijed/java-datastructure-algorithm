a959c342e9e58703d8b7c4e6fdc9d5aef01511ff
Jed Li <Jed.Li@ariix.com>
2021-06-08 15:07:28 +0800

    Initial commit

---
+++ src/main/java/com/me/datastructure/javadatastructurealgorithm/JavaDatastructureAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.me.datastructure.javadatastructurealgorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDatastructureAlgorithmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDatastructureAlgorithmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/me/datastructure/javadatastructurealgorithm/JavaDatastructureAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.datastructure.javadatastructurealgorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDatastructureAlgorithmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
